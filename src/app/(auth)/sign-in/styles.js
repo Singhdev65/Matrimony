c00521446633a9b770cf0395549c4b8b15d25d04
Swatiparna <swatiparna@etelligens.in>
2024-06-17 10:49:56 +0530

    comments removed

--- src/app/(auth)/sign-in/styles.js
+++ src/app/(auth)/sign-in/styles.js
@@ -14,15 +14,16 @@ export const LoginContainer = styled.div`
 `;
 
 export const LeftSection = styled.div`
-  flex: 0.25;
+  flex: 0.35;
   background-color: red;
   ${"" /* width: 30vw; */}
   ${"" /* height: 100vh; */}
 `;
 
 export const RightSection = styled.div`
-  flex: 0.75;
-  ${'' /* background-color: green; */}
+  flex: 0.7;
+  padding: 30px;
+  ${"" /* background-color: green; */}
   ${"" /* width: 70vw; */}
   ${"" /* height: 100vh; */}
 `;
@@ -37,18 +38,30 @@ export const Card = styled.div`
   background-color: white;
   height: 100%;
   border-radius: 20px;
-  border: 1px solid blue;
   height: 100vh;
+  z-index: 1;
 `;
 
-export const LoginTabs = styled.div`
+export const LoginOptions = styled.div`
   display: flex;
   margin-bottom: 20px;
 `;
 
-export const Tab = styled.button`
+export const RadioButton = styled.button`
   cursor: pointer;
   padding: 10px;
+  border: none;
+  background-color: #fff;
+  input[type="radio"] {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+  }
+
+  label {
+    cursor: pointer;
+  }
+
   &.active {
     background: blue;
     color: white;
@@ -56,4 +69,73 @@ export const Tab = styled.button`
 `;
 export const TabContent = styled.div`
   margin-top: 20px;
+`;
+export const Background = styled.div`
+  position: relative;
+  overflow-y: hidden;
+`;
+
+export const EmailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const Input = styled.input`
+  margin-bottom: 10px;
+  margin-top: 10px;
+  display: block;
+  width: 100%;
+  height: 1.5rem;
+  padding: 1rem 1rem;
+  font-family: "main-font-family";
+  font-size: 0.75rem;
+  font-weight: normal;
+  line-height: 1.25;
+  color: #343434;
+  background-color: #fffafa;
+  background-clip: padding-box;
+  border: 1px solid #ececec;
+  border-radius: 0.5rem;
+  box-shadow: none;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+`;
+
+export const SignupButton = styled.button`
+  width: 30%;
+  padding: 1rem;
+  font-family: "main-font-family";
+  font-size: 0.75rem;
+  font-weight: normal;
+  line-height: 1.25;
+  color: #6d635e;
+  background-color: #fdf0e4;
+  border: 1px solid #ececec;
+  border-radius: 0.5rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  font-weight: medium;
+  margin-bottom: 4rem;
+
+  cursor: pointer;
+  &:hover {
+    background-color: #ddcac7;
+  }
+`;
+
+export const LoginButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40%;
+`;
+
+export const ForgotPassword = styled.h6`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  margin: 0;
+  padding: 0.5rem;
+  font-size: 0.875rem;
+  color: #007bff;
+  cursor: pointer;
 `;
